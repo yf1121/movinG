7e1b84985da47ce8ae9c4f8ef42bb2334f2f3f9f
FuruNov <clz14116@ict.nitech.ac.jp>
2021-05-04 12:27:26 +0900

    add comment section

--- electron-readus/main.js
+++ electron-readus/main.js
@@ -1,22 +1,54 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
-let mainWindow = null;
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCd8tr0y47lvUhyL7NWT112epaxWXsMABw",
+  authDomain: "moving-5e9c4.firebaseapp.com",
+  projectId: "moving-5e9c4",
+  storageBucket: "moving-5e9c4.appspot.com",
+  messagingSenderId: "1032830265053",
+  appId: "1:1032830265053:web:f989c3ee6e6fb588669d34",
+  measurementId: "G-Z7JJY20GS8"
+};
 
-app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-app.on('ready', function() {
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
 
-  // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
