f639f946c34e6fb02b7ed02fafb0035026a7c793
FuruNov <clz14116@ict.nitech.ac.jp>
2021-05-08 20:49:09 +0900

    add electron-firebase

--- electron-readus/push_button/push_emoji.js
+++ electron-readus/push_button/push_emoji.js
@@ -1,17 +1,17 @@
-// const { db } = require("./firebase");
-let button = document.getElementById('button1')
-button.onclick = function() {
+let button_e = document.getElementById('button1')
+button_e.onclick = function() {
     button1.style.backgroundColor = "lightblue";
 
     const balloon_elem = document.createElement("div")
-    balloon_elem.classList.add("balloon")
+    balloon_elem.classList.add("balloon_emoji")
+
     balloon_elem.innerHTML =
         '<span><svg viewBox="0 0 300 400">'
         + '<path d="M299.9,178C303.6,69.3,235.8,0,150,0C64.2,0-3.5,69.3,0.1,178C4,291.2,90.2,361.3,140.6,376.9l-9.2,22.8h37.2l-9.2-22.8C209.8,361.4,296,291.2,299.9,178z"></path>'
         +'</svg></span>'
+
     document.body.appendChild(balloon_elem)
     sleep(5, function () {
-        // alert('けす！');
         balloon_elem.remove();
     });
 
